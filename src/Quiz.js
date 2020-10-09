465a1217fb5c860507df2f2838a555cdf0d0324c
kwuaa <kwuaa@allstate.com>
2020-10-09 02:51:20 +0100

    Update Quiz.js

--- src/Quiz.js
+++ src/Quiz.js
@@ -6,29 +6,22 @@ let quizData = require('./quiz_data.json')
 
 class Quiz extends Component {
     constructor(props) {
-        super(props);
-        this.state = {quiz_position: 1}
-        this.showNextQuestion  = this.showNextQuestion.bind(this);
+      super(props)
+      this.state = { quiz_position: 1 }
     }
-
-    render() {
-        console.log("rendering");
-        //const isQuizEnd = (quizData.quiz_questions.length === (this.state.quiz_position-1))
-        const isQuizEnd = ((this.state.quiz_position - 1) === quizData.quiz_questions.length)
-
-        return ( 
-            <div>
-                {isQuizEnd ? <QuizEnd /> : <QuizQuestion quiz_question={quizData.quiz_questions[this.state.quiz_position - 1]} showNextQuestionHandler={this.showNextQuestion.bind(this)} />}
-            </div>
-        )
-    }
-
     showNextQuestion() {
-        console.log("change state");
-        this.setState((state) => {
-          return { quiz_position: state.quiz_position + 1 }
-        })
-     }
-}
+      this.setState((state) => {
+        return { quiz_position: state.quiz_position + 1 }
+      })
+    }
+    render() {
+      const isQuizEnd = ((this.state.quiz_position - 1) === quizData.quiz_questions.length)
+      return (
+        <div>
+          {isQuizEnd ? <QuizEnd /> : <QuizQuestion quiz_question={quizData.quiz_questions[this.state.quiz_position - 1]} showNextQuestionHandler={this.showNextQuestion.bind(this)} />}
+        </div>
+      )
+    }
+  }
 
 export default Quiz
