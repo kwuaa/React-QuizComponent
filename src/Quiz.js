de0721a8928fab47539347c4361469718433d19b
kwuaa <kwuaa@allstate.com>
2020-10-09 02:28:33 +0100

    Update Quiz.js

--- src/Quiz.js
+++ src/Quiz.js
@@ -8,18 +8,22 @@ class Quiz extends Component {
     constructor(props) {
         super(props);
         this.state = {quiz_position: 1}
+        this.showNextQuestion  = this.showNextQuestion.bind(this);
     }
 
     render() {
         //const isQuizEnd = (quizData.quiz_questions.length === (this.state.quiz_position-1))
         const isQuizEnd = ((this.state.quiz_position - 1) === quizData.quiz_questions.length)
 
-        return (
-            
-                <div>
-                    {isQuizEnd ? <QuizEnd /> : <QuizQuestion quiz_question={quizData.quiz_questions[this.state.quiz_position-1]}/>}
-                </div>
+        return (            
+            <div>
+                {isQuizEnd ? <QuizEnd /> : <QuizQuestion quiz_question={quizData.quiz_questions[this.state.quiz_position-1]}/>}
+            </div>
         )
+    }
+
+    showNextQuestion() {
+        console.log(this.props);
     }
 }
 
