d2d2d068cf42cf609ff60fce87eff79205623f27
kwuaa <kwuaa@allstate.com>
2020-10-09 02:41:42 +0100

    Update QuizQuestionButton.js

--- src/QuizQuestionButton.js
+++ src/QuizQuestionButton.js
@@ -2,20 +2,17 @@ import React, { Component } from 'react'
 
 class QuizQuestionButton extends Component {
 
-    constructor(props) {
-        super(props);
-        this.handleClick   = this.handleClick.bind(this);
-    }
-
     render() {
         return (
-            <li><button onClick={() => this.handleClick('me') }>{this.props.button_text}</button></li>
-        );
+            <li>
+                <button onClick={this.handleClick.bind(this)}>{this.props.button_text}</button>
+            </li>
+        )
     }   
 
     handleClick () {
-        console.log(this.props.button_text);
-        //console.log(button_text);
+        console.log("handleClick in Button");
+        this.props.clickHandler(this.props.button_text)
     }
 }
 
